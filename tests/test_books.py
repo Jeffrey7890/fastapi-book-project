2920705520692715162dc2403302fc9b96ee9782
Jeffrey7890 <d3v3loperpy@gmail.com>
2025-02-12 11:18:16 +0000

    Test for invalid book_id=999

--- tests/test_books.py
+++ tests/test_books.py
@@ -14,6 +14,13 @@ def test_get_single_book():
     assert data["title"] == "The Hobbit"
     assert data["author"] == "J.R.R. Tolkien"
 
+def test_get_invalid_book():
+    response = client.get("/books/999")
+    assert response.status_code == 404
+    data = response.json()
+    assert data['detail'] == 'Book Not Found'
+
+
 
 def test_create_book():
     new_book = {
